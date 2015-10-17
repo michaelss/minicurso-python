669c0f855c0cdb9c31646e9b1f5751456583ad24
Michael Schuenck <michaelss@gmail.com>
2015-10-17 11:28:37 -0300

    Detalhamentos em módulos.

--- src/modulos/calculadora.py
+++ src/modulos/calculadora.py
@@ -1,9 +1,16 @@
 '''Módulo com operações matemáticas.'''
 
 def somar(op1, op2):
-    '''Operação para somar dois números.'''
+    '''Função para somar dois números.'''
     return op1 + op2
 
 def subtrair(op1, op2):
-    '''Operação para subtrair o segundo número do primeiro.'''
+    '''Função para subtrair o segundo número do primeiro.'''
     return op1 - op2
+
+def dividir(op1, op2):
+    '''Função dividir o primeiro pelo segundo operador.'''
+    try:
+        return op1 / op2
+    except ZeroDivisionError:
+        print('Não se pode dividir por 0.')
--- src/modulos/principal.py
+++ src/modulos/principal.py
@@ -11,3 +11,16 @@ from calculadora import subtrair
 res = subtrair(1, 2)
 
 print(res)
+
+
+
+
+from calculadora import dividir as d
+
+res = d(20, 4)
+
+print(res)
+
+res = d(2, 0)
+
+print(res)
